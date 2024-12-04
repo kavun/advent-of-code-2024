560d7e8915c8df30b84561802329811ffb73d407
Kevin Reed <662069+kavun@users.noreply.github.com>
2024-12-03 21:20:20 -0600

    run

--- aoc.ps1
+++ aoc.ps1
@@ -1,18 +1,18 @@
 param(
-  [Parameter(Position=0, Mandatory=$True)]
-  [ValidateSet("run", "test")]
-  [string]$Command
+    [Parameter(Position = 0, Mandatory = $True)]
+    [ValidateSet('run', 'test')]
+    [string]$Command
 )
 
 function Invoke-Run() {
-    & dotnet run --project "$PSScriptRoot/src/adventofcode2024.console/adventofcode2024.console.csproj"
+    & dotnet run -c Release --project "$PSScriptRoot/src/adventofcode2024.console/adventofcode2024.console.csproj"
 }
 
 function Invoke-Test() {
-    & dotnet test
+    & dotnet run -c Release --project "$PSScriptRoot/tests/adventofcode2024.UnitTests/adventofcode2024.UnitTests.csproj"
 }
 
 switch ($Command) {
-    "run"    { Invoke-Run }
-    "test"  { Invoke-Test }
+    'run' { Invoke-Run }
+    'test' { Invoke-Test }
 }
