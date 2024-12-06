e5e22b6962feca9304f20e68a0fbeab95fbf5192
Kevin Reed <662069+kavun@users.noreply.github.com>
2024-12-05 21:51:52 -0600

    Day03 Part1

---
+++ src/adventofcode2024/Day03.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace adventofcode2024;
+
+public class Day03 : IDay
+{
+    public Answer Solve(IEnumerable<string> lines)
+    {
+        var part1 = 0L;
+        var part2 = 0L;
+
+        part1 = lines.Sum(line =>
+            Regex.Matches(line, @"mul\(([0-9]+),([0-9]+)\)")
+                .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value)));
+
+        return new Answer()
+        {
+            Part1 = part1,
+            Part2 = part2
+        };
+    }
+}
---
+++ tests/adventofcode2024.UnitTests/Day03Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace adventofcode2024.UnitTests;
+
+public class Day03Tests
+{
+    [Test]
+    [Arguments("""
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        """, 161, 0)]
+    public async Task Solve(string input, long expectedPart1, long expectedPart2)
+    {
+        var actualAnswer = new Day03().Solve(input.Split(Environment.NewLine));
+
+        await Assert.That(actualAnswer.Part1).IsEqualTo(expectedPart1);
+        await Assert.That(actualAnswer.Part2).IsEqualTo(expectedPart2);
+    }
+}
