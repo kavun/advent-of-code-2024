ad63f5b2f8f4c829da3834bd51e57a36312f4e45
Kevin Reed <662069+kavun@users.noreply.github.com>
2024-12-09 22:27:27 -0600

    Day05 Part1

---
+++ src/adventofcode2024/Day05.cs
@@ -0,0 +1,74 @@
+﻿namespace adventofcode2024;
+
+public partial class Day05 : IDay
+{
+    public Answer Solve(IEnumerable<string> lines)
+    {
+        var part1 = 0L;
+        var part2 = 0L;
+
+        var section1 = true;
+        var mustBeBefores = new Dictionary<int, HashSet<int>>();
+        var cannotBeBefores = new Dictionary<int, HashSet<int>>();
+        var validMids = new List<int>();
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                section1 = false;
+                continue;
+            }
+
+            if (section1)
+            {
+                var parts = line.Split("|");
+                var a = int.Parse(parts[0]);
+                var b = int.Parse(parts[1]);
+                mustBeBefores.TryAdd(a, new HashSet<int>());
+                mustBeBefores[a].Add(b);
+                cannotBeBefores.TryAdd(b, new HashSet<int>());
+                cannotBeBefores[b].Add(a);
+            }
+            else
+            {
+                var parts = line
+                    .Split(",")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var leftParts = new HashSet<int>();
+                var rightParts = new HashSet<int>(parts.Skip(1));
+                var partValid = true;
+
+                for (var i = 0; i < parts.Length; i++)
+                {
+                    var num = parts[i];
+                    var mustBeBefore = mustBeBefores.GetValueOrDefault(num, new HashSet<int>());
+                    var cannotBeBefore = cannotBeBefores.GetValueOrDefault(num, new HashSet<int>());
+
+                    var numValid = mustBeBefore.All(m => !leftParts.Contains(m))
+                        && cannotBeBefore.All(m => !rightParts.Contains(m));
+                    if (!numValid)
+                    {
+                        partValid = false;
+                        break;
+                    }
+
+                    leftParts.Add(num);
+                    rightParts = new HashSet<int>(parts.Skip(i + 1));
+                }
+
+                if (partValid)
+                {
+                    part1 += parts[(int)Math.Floor(parts.Length / 2m)];
+                }
+            }
+        }
+
+        return new Answer()
+        {
+            Part1 = part1,
+            Part2 = part2
+        };
+    }
+}
---
+++ tests/adventofcode2024.UnitTests/Day05Tests.cs
@@ -0,0 +1,43 @@
+﻿namespace adventofcode2024.UnitTests;
+
+public class Day05Tests
+{
+    [Test]
+    [Arguments("""
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+        """, 143, 0)]
+    public async Task Solve(string input, long expectedPart1, long expectedPart2)
+    {
+        var actualAnswer = new Day05().Solve(input.Split(Environment.NewLine));
+
+        await Assert.That(actualAnswer.Part1).IsEqualTo(expectedPart1);
+        await Assert.That(actualAnswer.Part2).IsEqualTo(expectedPart2);
+    }
+}
