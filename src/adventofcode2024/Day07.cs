3c4b4e8831fd326596aec5fe98c0805623f99e98
Kevin Reed <662069+kavun@users.noreply.github.com>
2024-12-10 20:51:48 -0600

    day7 start

---
+++ src/adventofcode2024/Day07.cs
@@ -0,0 +1,16 @@
+namespace adventofcode2024;
+
+public partial class Day07 : IDay
+{
+    public Answer Solve(IEnumerable<string> lines)
+    {
+        var part1 = 0L;
+        var part2 = 0L;
+
+        return new Answer()
+        {
+            Part1 = part1,
+            Part2 = part2
+        };
+    }
+}
---
+++ tests/adventofcode2024.UnitTests/Day07Tests.cs
@@ -0,0 +1,24 @@
+namespace adventofcode2024.UnitTests;
+
+public class Day07Tests
+{
+    [Test]
+    [Arguments("""
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        """, 3749, 0)]
+    public async Task Solve(string input, long expectedPart1, long expectedPart2)
+    {
+        var actualAnswer = new Day07().Solve(input.Split(Environment.NewLine));
+
+        await Assert.That(actualAnswer.Part1).IsEqualTo(expectedPart1);
+        await Assert.That(actualAnswer.Part2).IsEqualTo(expectedPart2);
+    }
+}
